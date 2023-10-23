1405d1e7f6594add85c271c8dedad51d751f9246
cti22d206 <haiduc.sebastian@stuent.uoradea.ro>
2023-10-23 20:39:09 +0300

    lab32

--- lab3/Probleme.java
+++ lab3/Probleme.java
@@ -1,13 +1,28 @@
 package lab3;
 
 public class Probleme {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         problema1();
+        problema2();
     }
-    private static void problema1(){
-        String cuvant="message";
-        for( int i = cuvant.length()-1 ; i>=0; i--){
+
+    private static void problema1() {
+        String cuvant = "message";
+        for (int i = cuvant.length() - 1; i >= 0; i--) {
             System.out.println(cuvant.charAt(i));
         }
     }
+
+    private static void problema2() {
+        String[] arr1 = {"java", "test", "university"};
+        String[] arr2 = {"car", "university", "plane"};
+        for(int i = 0; i<arr1.length; i++){
+            {
+                for( int j = 0; j<arr2.length; j++ ){
+                     System.out.println(arr1[i].equals(arr2[j]));
+                }
+            }
+        }
+    }
+
 }
