946a9e66340729473c3fb64860b334f715070fed
cti22d206 <haiduc.sebastian@stuent.uoradea.ro>
2023-10-23 20:43:42 +0300

    lab32

--- lab3/Probleme.java
+++ lab3/Probleme.java
@@ -18,8 +18,11 @@ public class Probleme {
         String[] arr2 = {"car", "university", "plane"};
         for(int i = 0; i<arr1.length; i++){
             {
-                for( int j = 0; j<arr2.length; j++ ){
-                     System.out.println(arr1[i].equals(arr2[j]));
+                for( int j = 0; j<arr2.length; j++){
+                    if(arr1[i].equals(arr2[j])) {
+                        System.out.println(arr1[i]);
+                    }
+
                 }
             }
         }
